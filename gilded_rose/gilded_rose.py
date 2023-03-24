ab2820bc6f78300e84aa2779358aac1e03f828ab
daviola <18071455+daviola@users.noreply.github.com>
2023-03-24 13:01:06 -0300

    refactoring the code for readability

--- gilded_rose/gilded_rose.py
+++ gilded_rose/gilded_rose.py
@@ -7,38 +7,36 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.name != "Sulfuras, Hand of Ragnaros":
-                    if item.quality > 0:
-                        item.quality = item.quality - 1 #dextery, elixir, conjured
-                        if item.name == "Conjured Mana Cake":
-                            item.quality = item.quality -1
-            else:
+            if item.name in ["+5 Dexterity Vest", "Elixir of the Mongoose"]:
+                if item.quality > 0:
+                    item.quality = item.quality - 1
+                if item.quality > 0 and item.sell_in < 0:
+                    item.quality = item.quality - 1
+
+            if item.name == "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+                if item.sell_in < 11:
+                    if item.quality < 50:
+                        item.quality = item.quality + 1
+                if item.sell_in < 6:
+                    if item.quality < 50:
+                        item.quality = item.quality + 1
                                 
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1 # dextery, elixir, conjured
-                                if item.name == "Conjured Mana Cake":
-                                    item.quality = item.quality -1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            
+            if item.name == "Conjured Mana Cake":
+                if item.quality > 0:
+                    item.quality = item.quality -2
+                if item.quality > 0 and item.sell_in < 0:
+                    item.quality = item.quality - 4
+                            
+            if item.name == "Aged Brie":
+                if item.quality < 50:
+                    item.quality = item.quality + 1
+                if item.sell_in < 0 and item.quality < 50:
+                    item.quality = item.quality + 1
 
 
 class Item:
